d4002975d6e670f616c10be711d5db6285c3865e
Luis Alfaro <52721684+luisguztav20@users.noreply.github.com>
2023-03-18 21:53:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ window.addEventListener('load', function(){
     console.log("Contenido cargado")
 
     var imagenes = [];
-    imagenes[0] = 'img/v1.jpg';
-    imagenes[1] = 'img/v2.jpg';
-    imagenes[2] = 'img/v3.jpg';
-    imagenes[3] = 'img/v4.jpg';
+    imagenes[0] = '/img/v1.jpg';
+    imagenes[1] = '/img/v2.jpg';
+    imagenes[2] = '/img/v3.jpg';
+    imagenes[3] = '/img/v4.jpg';
     var tiempo = 5000;
     var indiceImagenes = 0;
     function cambiarImagenes(){
@@ -25,10 +25,10 @@ window.addEventListener('load', function(){
     console.log("Contenido cargado")
 
     var imagenes = [];
-    imagenes[0] = 'img/h1.jpg';
-    imagenes[1] = 'img/h2.jpg';
-    imagenes[2] = 'img/h3.jpg';
-    imagenes[3] = 'img/h4.jpg';
+    imagenes[0] = '/img/h1.jpg';
+    imagenes[1] = '/img/h2.jpg';
+    imagenes[2] = '/img/h3.jpg';
+    imagenes[3] = '/img/h4.jpg';
     var tiempo = 5000;
     var indiceImagenes = 0;
     function cambiarImagenes(){
